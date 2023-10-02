704a7e60f3baba7b78429fb2e43f6a9bc05aa21e
YgorRupp <ygor.rupp@hotmail.com>
2023-10-02 07:35:10 -0300

    finish create list paginated

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,36 @@ import axios from "axios";
 const app: Application = express();
 app.use(cors());
 
+const token = process.env.TOKEN;
+
+app.get("/movies/:page", async (req, res) => {
+  const page: number = parseInt(req.params.page);
+
+  if (isNaN(page) || page < 1 || page > 500) {
+    return res.status(400).json({
+      error:
+        "Página inválida. As páginas devem ser números inteiros entre 1 e 500.",
+    });
+  }
+
+  try {
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?page=${page}`,
+      {
+        headers,
+      }
+    );
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro ao acessar a API externa" });
+  }
+});
+
 app.use(express.json());
 
 export default app;
